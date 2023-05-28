d42c53a59868349f939ba04f4880769642336fb4
t4wefan <66654403+t4wefan@users.noreply.github.com>
2023-05-28 13:00:20 +0800

    Update read_preset.py

--- read_preset.py
+++ read_preset.py
@@ -1,5 +1,6 @@
 import os
 import json
+from maxid import count_presets
 
 def read_preset(id):
     filename = f"presets/{id}.json"
@@ -9,4 +10,6 @@ def read_preset(id):
         with open(filename, "r") as f:
             preset_data = json.load(f)
     preset_data["status"] = status
+    preset_data["max_id"] = count_presets()
+
     return preset_data
